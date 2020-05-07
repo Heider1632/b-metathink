85d11cd33907c66f17a7351c64e9fe0bf64383df
Heider Zapa <heiderzapa78@gmail.com>
2020-05-07 14:13:27 -0500

    cognitiveModel changed

--- models/cognitiveModel.js
+++ models/cognitiveModel.js
@@ -4,7 +4,7 @@ const cognitiveModelSchema = new Schema({
     User: { type: Schema.Types.ObjectId, ref:"User", required: true },
     createdAt: { type: Date, default: Date.now },
     Goal: [{ type: String }],
-    StateMental: [{ type: String }],
+    MentalState: [{ type: String }],
     Action: [{ type: String }],
     Object: [{ type: String }],
     domain: { type: String, required: true },
